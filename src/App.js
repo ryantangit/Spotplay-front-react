10f587af4e4c125bfd2d1803e65a7b0f820c25b9
Ryan Tan <ryantan.mail@gmail.com>
2023-04-28 23:17:27 -0600

    i want to have the green square for today but i ended up sleeping

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+			This is a paragraph and spotify soon will be in here.
+			I don't know how but it will be done.
+        </p> 
       </header>
     </div>
   );
