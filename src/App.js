a1d7689aef4c16419496f029c60a90844cbcf22d
Ryan Tan <ryantan.mail@gmail.com>
2023-05-01 23:00:16 -0600

    tmrw most def get some stuff done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <p>
 			This app will identify some fun things about you and your spotify usage.
+			<a href="http:localhost:3000/login">Login to Spotify</a>
         </p> 
       </header>
     </div>
